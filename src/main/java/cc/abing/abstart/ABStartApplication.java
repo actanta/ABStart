f4ba8ba29e9f43563a57f7c21f78e6e1fd2af17d
actanta <abingcc@yeah.net>
2022-05-25 14:16:47 +0800

    feat:初始化项目

---
+++ src/main/java/cc/abing/abstart/ABStartApplication.java
@@ -0,0 +1,13 @@
+package cc.abing.abstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ABStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/abing/abstart/ABStartApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.abing.abstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ABStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
